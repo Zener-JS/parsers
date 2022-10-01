778dcdb038999f64eca977a583c3357b5cb00c6c
Jothin-kumar <contact@jothin.tech>
2022-10-01 15:42:01 +0530

    Remove leading and trailing whitespaces, newlines and comments from CSS.

---
+++ src/css/__init__.py
@@ -0,0 +1,61 @@
+"""
+MIT License
+
+Copyright (c) 2021 Jothin Kumar
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+Author(s): Jothin Kumar <contact@jothin.tech>
+GitHub repository: https://github.com/Zener-JS/parsers
+"""
+
+
+class Parser:
+    def __init__(self, raw_css: str):
+
+        # Remove leading whitespaces, trailing whitespaces, newlines
+        self.raw_css = ""
+        for line in raw_css.split("\n"):
+            self.raw_css += line.strip()
+        self.raw_css = self.raw_css.replace("\n", "")
+
+        # CSS pre parsing
+        self.raw_css.replace("\\\'", "__parser__ single-quote")
+        self.raw_css.replace("\\\"", "__parser__ double-quote")
+
+        # Remove comments
+        chr_count = {
+            "\"": 0,
+            "\'": 0
+        }
+        while self.raw_css.partition("/*")[1] == "/*":
+            partition = list(self.raw_css.partition("/*"))
+            chr_count["\""] = len(partition[0]) - len(partition[0].replace("\"", ""))
+            chr_count["\'"] = len(partition[0]) - len(partition[0].replace("\'", ""))
+            if chr_count["\""] % 2 == chr_count["\'"] % 2 == 0:
+                self.raw_css = partition[0] + partition[2].partition("*/")[2]
+            else:
+                if partition[1] == "/*":
+                    partition[1] = "__parser__ start-comment"
+                self.raw_css = "".join(partition)
+        self.raw_css = self.raw_css.replace("__parser__ start-comment", "/*")
+
+        # CSS post parsing
+        self.raw_css.replace("__parser__ single-quote", "\\\'")
+        self.raw_css.replace("__parser__ double-quote", "\\\"")
