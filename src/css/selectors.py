13a7376845e5c06ddfc68c5c64c63ee75d618b7b
Jothin-kumar <contact@jothin.tech>
2022-10-02 15:27:07 +0530

    Improve new_current_selectors in selectors.py

--- src/css/selectors.py
+++ src/css/selectors.py
@@ -89,15 +89,13 @@ def new_current_selectors(new_selectors: str, test=False):
         i += 1
     global _current_selectors
     _current_selectors = []
-    while new_selectors.partition(",")[1] == ",":
+    while new_selectors:
         partition = new_selectors.partition(",")
         if (len(partition[0]) - len(partition[0].replace("\"", ""))) % 2 == (len(partition[0]) - len(partition[0].replace("\'", ""))) % 2 == 0:
             _current_selectors.append(partition[0].strip().replace(comma_str, ","))
             new_selectors = partition[2]
         else:
             new_selectors = f"{partition[0]}{comma_str}{partition[2]}"
-    if new_selectors.strip():
-        _current_selectors.append(new_selectors.strip().replace(comma_str, ","))
     if test:
         return _current_selectors
 
